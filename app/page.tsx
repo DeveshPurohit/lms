56e33ad1d17d0fef65476a8e614740e493213fd0
DeveshPurohit <purohitdev08@gmail.com>
2025-09-27 18:32:50 +0530

    supabase, clerk and companion form and creation implementeed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,49 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div><h1 className='text-3xl font-bold underline text-center mt-10'>
-        Lets Go Boy! Devesh OP
-      </h1></div>
-  )
+    <main>
+      <h1>Lets Go Boy! Devesh OP</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Three musketers"
+          topic="2 grils gang that attacked the villains"
+          subject="Arcade"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="123"
+          name="Three musketers"
+          topic="2 grils gang that attacked the villains"
+          subject="Arcade"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="123"
+          name="Three musketers"
+          topic="2 grils gang that attacked the villains"
+          subject="Arcade"
+          duration={45}
+          color="#ffda6e"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
 }
 
 export default Page
---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,10 @@
+import { PricingTable } from '@clerk/nextjs'
+import React from 'react'
+
+const Subscriptions = () => {
+  return (
+    <div><PricingTable/></div>
+  )
+}
+
+export default Subscriptions
